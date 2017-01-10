d7d9e3e795ad63e912873b5497a5a399b488b687
pachauri ryan <rpachauri@gmail.com>
2017-01-10 15:20:16 -0800

    Complete some of the work for module six

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,15 +2,18 @@
 
 # Define a function `AddThree` that takes a single argument and
 # returns a value 3 greater than that input
-
+AddThree <- function(INPUT) 
+  INPUT + 3
 
 # Create a variable `ten` that is the result of passing 7 to your `AddThree` function
-
+ten <- AddThree(7)
 
 # Define a function `ImperialToMetric` that takes in two arguments: a number of feet
 # and a number of inches
 # The function should return the total length in meters
-
+ImperialToMetric <- function(num.feet, num.inches)
+  num.feet * 0.3048 + num.inches * 0.0254
 
 # Create a variable `height.in.meters` by passing your height in imperial to the
 # `ImperialToMetric` function
+height.in.meters = ImperialToMetric(5, 9)
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -3,14 +3,21 @@
 # Define a function `CompareLength` that takes in 2 character strings, and returns
 # the sentence of the form
 #   "The difference in length is N"
-
+CompareLength <- function(first.char.string, second.char.string)
+  "The difference in length is " + nchar(first.char.string) - nchar(second.char.string)
 
 # Pass two strings of different lengths to your `CompareLength` function
-
+CompareLength("this is the first string",
+              "this is the second string that will be used to compare length")
 
 # Define a function `DescribeDifference` that will return one of the following statements:
 # "Your first string is longer by N characters"
 # "Your second string is longer by N characters"
-
+DescribeDifference <- function(first.char.string, second.char.string)
+  diff = nchar(first.char.string) - nchar(second.char.string)
+  if (diff > 0)
+    "Your first string is longer by " diff " characters"
+  else
+    "Your second string is longer by " (-1 * diff) " characters"
 
 # Pass two strings of different lengths to your `DescribeDifference` function
